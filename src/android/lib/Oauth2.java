f9b9cfdc660e0c7ef47edbad278568e85370dfc1
dmitry.a <dmitry.a@spikeglobal.com>
2023-03-20 14:58:02 +0000

    SLIV-1313

--- src/android/lib/Oauth2.java
+++ src/android/lib/Oauth2.java
@@ -39,7 +39,7 @@ import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.security;
+//import java.security;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
